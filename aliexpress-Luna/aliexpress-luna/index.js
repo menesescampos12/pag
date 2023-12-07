e7f34cbd98d3e00b958290146ac6750880b5da41
menesescampos12 <118848107+menesescampos12@users.noreply.github.com>
2023-12-06 18:53:14 -0600

    Add files via upload

---
+++ aliexpress-Luna/aliexpress-luna/index.js
@@ -0,0 +1,15 @@
+// Mostrar una bienvenida cuando la página se cargue
+window.onload = function () {
+    alert('bienvenidos')
+}
+
+// Mostrar una bienvenida cuando la página se cargue
+window.onload = function () {
+    alert('¡Bienvenido a Aliexpress');
+}
+
+const destacado  = document.getElementById("container")
+    console.log(container);
+
+    const elementsByClassName = document.getElementsByClassName("container");
+    console.log(elementsByClassName);
